d0d661a3a9eb39e7a97a80c8c4e6496cc29fd68e
Andhika Nurrafli Putra <andhikanurrafli@gmail.com>
2024-12-16 22:45:02 +0700

    Add files via upload

---
+++ python/Untitled-1.py
@@ -0,0 +1,73 @@
+import math  #import fungsi(?)
+
+satu = 1
+
+
+print(satu)
+print("You've succesfully run a python code")
+
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+math.ceil(alice_candies)
+
+x001 = math.sqrt(carol_candies)
+
+print(x001)
+
+x00 = math.pi
+
+print(x00)
+
+to_smash = (alice_candies + bob_candies + carol_candies) %  3
+
+print(to_smash)
+print(to_smash + 2)
+
+x = "Aku String"
+xDataType = type(x)
+print(xDataType)
+
+x0 = {"Jeruk", "Apel", "Mangga"}
+
+kutipSatu = 'Kutip Satu ("Im Batman")'
+kutipDua = "Kutip Dua (I'm Iron Man)"
+kutipTiga = ''' 
+    "Im Batman"
+    I'm Iron Man
+'''
+
+print(kutipSatu)
+print(kutipDua)
+print(kutipTiga)
+
+txt = "Salam kenal murid \"Purwadhika\", mari kita belajar bersama."
+print(txt)
+
+x1 = 'Halo Dunia'
+
+print(len(x1))
+
+panjangString = len(x1)
+indexDunia = x1.index('Dunia')
+print(indexDunia)
+hasilSplit = x1.split(' ')
+print(hasilSplit)
+jadiHurufKecil = x1.lower()
+print(jadiHurufKecil)
+jadiHurufBesar = x1.upper()
+print(jadiHurufBesar)
+hurufPertamaKapital = x1.capitalize()
+print(hurufPertamaKapital)
+
+text = "I'm Baron nice to meet you"
+print(text[10:14])
+
+username = input("Masukkan username:")
+print("Username anda: " + username)
+
+x = 4
+y = 3
+z = 2
+
